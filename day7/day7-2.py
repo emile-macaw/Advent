5c31eb7997365666b3f69e9f387b9df472487817
Emile van Ewijk <Emile.van.Ewijk@macaw.nl>
2023-12-07 22:59:58 +0100

    day7

---
+++ day7/day7-2.py
@@ -0,0 +1,75 @@
+import os
+from collections import Counter
+
+cardsByValues = "J23456789TQKA"
+
+class hand:    
+
+    def __init__(self, cardString:str, bid:int):
+        self.cardString = cardString
+        self.bid = bid
+    
+    def getSetSizes(self):
+        counter = Counter(self.cardString)
+        
+        jokerCount = counter['J']
+        del counter['J']
+        
+        values = list(map(lambda d: d, counter.values()))        
+        sortedSets = sorted(values, reverse=True)
+        
+        if jokerCount < len(self.cardString):
+            sortedSets[0] += jokerCount
+        else:
+            sortedSets.append(jokerCount)
+
+        return sortedSets
+
+    def getSortableValue(self):
+        print('calculate value for hand ' + self.cardString)
+
+        totalCardValue = 0
+        cardPos = 1
+        
+        for card in self.cardString:
+            cardValue = (cardsByValues.index(card) + 1) *  (len(cardsByValues) + 1) ** (len(self.cardString) + 1 - cardPos)
+            totalCardValue += cardValue
+            cardPos += 1
+
+            print('card ' + card + ' at pos ' + str(cardPos) + ' gets value ' + str(cardValue))
+
+        setSizes = self.getSetSizes()
+        primarySizeValue = setSizes[0] * (len(cardsByValues) + 1) ** (len(self.cardString) + 2)
+        print('primary size value ' + str(primarySizeValue))
+        secondarySizeValue = setSizes[1] * (len(cardsByValues) + 1) ** (len(self.cardString) + 1) if len(setSizes) > 1 else 0
+        print('secondary size value ' + str(secondarySizeValue))
+
+        sortableValue = primarySizeValue + secondarySizeValue + totalCardValue
+
+        print('total value ' + str(sortableValue))
+
+        return sortableValue        
+
+def parseInput():
+
+    dir = dir = os.path.dirname(__file__)
+    data =   open(os.path.join(dir, "input.txt"))
+
+    hands = []
+    for line in data:
+        cardString = line.split(' ')[0]
+        bid = int(line.split(' ')[1])
+        hands.append(hand(cardString, bid))
+    return hands
+
+hands = parseInput()
+hands.sort(key = lambda h:h.getSortableValue())
+
+rankIndex = 1
+totalRank = 0
+for hand in hands:
+    totalRank += hand.bid * rankIndex
+    print('hand with cards ' + hand.cardString + ' index ' + str(rankIndex) + ' bid ' + str(hand.bid) + ' rank ' + str(totalRank))
+    rankIndex += 1
+
+print(totalRank)
